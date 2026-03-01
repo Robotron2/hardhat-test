841582ab5198250336b9fdea18289ad472060357
Theophilus <theophilusadesola002@gmail.com>
2026-03-01 16:52:25 +0100

    fix: corrected token addresses

--- scripts/swapETHForExactToken.ts
+++ scripts/swapETHForExactToken.ts
@@ -2,8 +2,10 @@ const helpers = require("@nomicfoundation/hardhat-network-helpers")
 import { ethers } from "hardhat"
 const main = async () => {
 	const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-	const WETHAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+	const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+	const WETHAddress = "0xC02aaA39b223FE8D0A0E5C4F27eAD9083C756Cc2"
 	const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+
 	const TokenHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621"
 
 	await helpers.impersonateAccount(TokenHolder)
